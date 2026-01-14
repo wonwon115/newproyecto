6e08397864d389d9b0672f77cfc8d29d1ee0a2dc
wonwon115 <wonwon.000050@gmail.com>
2026-01-14 20:56:47 -0300

    13-1

--- new-proyect/src/components/Fooditem/Fooditem.jsx
+++ new-proyect/src/components/Fooditem/Fooditem.jsx
@@ -1,22 +1,22 @@
-import React, { useState } from 'react'
+import React, { useContext } from 'react'
 import './Fooditem.css'
 import { assets } from '../../assets/assets'
+import { StoreContext } from '../../context/StoreContext'
 
 const Fooditem = ({ id, name, price, description, image }) => {
 
-
-    const { itemCount, setItemCount } = useState(0)
+    const { cartItems, addToCart, removeFromCart } = useContext(StoreContext)
 
     return (
         <div className="food-item">
-            <div className="food-item-container">
+            <div className="food-item-img-container">
                 <img className='food-item-image' src={image} />
-                {!itemCount
-                    ? <img className='add' onClick={() => setItemCount(prev => prev + 1)} src={assets.add_icon_white} />
-                    : <div className="food-tiem-counter">
-                        <img onClick={() => setItemCount(prev => prev - 1)} src={assets.remove_icon_red} alt="" />
-                        <p>{itemCount}</p>
-                        <img onClick={() => setItemCount(prev => prev + 1)} src={assets.add_icon_white} alt="" />
+                {!cartItems[id]
+                    ? <img className='add' onClick={() => addToCart(id)} src={assets.add_icon_white} />
+                    : <div className="food-item-counter">
+                        <img onClick={() => removeFromCart(id)} src={assets.add_icon_green} alt="" />
+                        <p>{cartItems[id]}</p>
+                        <img onClick={() => addToCart(id)} src={assets.remove_icon_red} alt="" />
                     </div>
                 }
             </div>
--- new-proyect/src/context/StoreContext.jsx
+++ new-proyect/src/context/StoreContext.jsx
@@ -4,8 +4,28 @@ import { food_list } from "../assets/assets";
 export const StoreContext = createContext(null)
 
 const StoreContextProvider = (props) => {
+
+    const [cartItems, setCartItems] = useState({});
+
+    const addToCart = (itemId) => {
+        if (!cartItems[itemId]) {
+            setCartItems((prev) => ({ ...prev, [itemId]: 1 }))
+        }
+        else[
+            setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }))
+        ]
+    }
+    const removeFromCart = (itemId) => {
+        setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }))
+
+    }
+
     const contextValue = {
-        food_list
+        food_list,
+        cartItems,
+        setCartItems,
+        addToCart,
+        removeFromCart,
     }
 
     return (
